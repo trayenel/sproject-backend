f89eaa4f4055f80b58bd86ee7e9a23621502a94a
trayenel <agent_tygae@yahoo.com>
2024-07-08 13:05:17 +0300

    Refactor: configured biome.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { HelloController } from './controllers/hello.controller'
 
 @Module({
   imports: [],
