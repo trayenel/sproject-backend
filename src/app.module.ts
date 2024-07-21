9e245c4a8b56f640badca4fc49cec125c92f2e3e
trayenel <agent_tygae@yahoo.com>
2024-07-21 13:57:16 +0300

    Added first route.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { HelloController } from './controllers/hello.controller'
+import {HelloController} from "./controllers/hello.controller";
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [HelloController],
   providers: [AppService],
 })
+
 export class AppModule {}
